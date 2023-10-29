496c5d39e3633a5f70d68d43958ac1adcb6ca0e0
Valaczka János Pál <valaczka.janos@piarista.hu>
2023-10-29 11:54:46 +0100

    Fix QDeferred

--- version/version.h
+++ version/version.h
@@ -2,6 +2,6 @@
 #define _VERSION_H_
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 4
-#define VERSION_BUILD 272
-#define VERSION_FULL "3.4.272"
+#define VERSION_BUILD 273
+#define VERSION_FULL "3.4.273"
 #endif
