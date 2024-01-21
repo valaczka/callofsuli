1e1522cc91819a3a16826d1c192a809be39814cd
Valaczka János Pál <valaczka.janos@piarista.hu>
2024-01-21 11:23:47 +0100

    Bugfix: ScoreList.qml empty class list

--- version/version.h
+++ version/version.h
@@ -2,6 +2,11 @@
 #define _VERSION_H_
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 6
+<<<<<<< Updated upstream
 #define VERSION_BUILD 24
 #define VERSION_FULL "3.6.24"
+=======
+#define VERSION_BUILD 48
+#define VERSION_FULL "3.6.48"
+>>>>>>> Stashed changes
 #endif
--- version/version.pri
+++ version/version.pri
@@ -1,4 +1,9 @@
 VER_MAJ = 3
 VER_MIN = 6
+<<<<<<< Updated upstream
 VER_PAT = 24
 VERSION = 3.6.24
+=======
+VER_PAT = 48
+VERSION = 3.6.48
+>>>>>>> Stashed changes
