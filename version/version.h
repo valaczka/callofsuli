bafcbb62c67a1890a857b382f9bea23dc650c4d3
Valaczka János Pál <valaczka.janos@piarista.hu>
2024-09-22 18:19:06 +0200

    Fix version number

--- version/version.h
+++ version/version.h
@@ -1,7 +1,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 #define VERSION_MAJOR 4
-#define VERSION_MINOR 2
-#define VERSION_BUILD 106
-#define VERSION_FULL "4.2.106"
+#define VERSION_MINOR 3
+#define VERSION_BUILD 0
+#define VERSION_FULL "4.3.0"
 #endif
