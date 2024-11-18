73b0d15c850421e038f36e71457d1d67b1b80525
Valaczka János Pál <valaczka.janos@piarista.hu>
2024-11-18 15:00:20 +0100

    Restore LiteGame extra time feature

--- version/version.h
+++ version/version.h
@@ -2,6 +2,6 @@
 #define _VERSION_H_
 #define VERSION_MAJOR 4
 #define VERSION_MINOR 3
-#define VERSION_BUILD 168
-#define VERSION_FULL "4.3.168"
+#define VERSION_BUILD 171
+#define VERSION_FULL "4.3.171"
 #endif
