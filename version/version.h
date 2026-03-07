08839dedbfb41022ccbe1370af62b8fea8eefc19
Valaczka János Pál <valaczka.janos@piarista.hu>
2026-03-07 18:47:49 +0100

    Bugfix: android full screen issue

--- version/version.h
+++ version/version.h
@@ -2,6 +2,6 @@
 #define _VERSION_H_
 #define VERSION_MAJOR 5
 #define VERSION_MINOR 2
-#define VERSION_BUILD 148
-#define VERSION_FULL "5.2.148"
+#define VERSION_BUILD 221
+#define VERSION_FULL "5.2.221"
 #endif
